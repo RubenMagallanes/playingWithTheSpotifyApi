613974fdc175255a27b428754e1f219d450b1b8f
RubenM <rubenm@datacom.co.nz>
2018-09-10 15:53:26 +1200

    add new script, fix redirects

--- getPlaylistTracks.sh
+++ getPlaylistTracks.sh
@@ -1,6 +1,6 @@
 curl \
-	-X GET "https://api.spotify.com/v1/playlists/$(<playlistId)" \
+	-X GET "https://api.spotify.com/v1/playlists/$(<playlistId)/tracks" \
 	-H "Accept: application/json" \
 	-H "Content-Type: application/json" \
 	-H "Authorization: Bearer $(<token)" \
-> playlist
+> playlistTracks
--- requestAppAuth.sh
+++ requestAppAuth.sh
@@ -3,5 +3,4 @@ curl \
 	-H "Authorization: Basic $(<base64client)" \
 	-d grant_type=client_credentials \
 	https://accounts.spotify.com/api/token \
-
 > authResponse
