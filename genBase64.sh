c3927e7b6adcecd9701888561c18b75d63ab4bdf
RubenMagallanes <rubenmmm3@gmail.com>
2018-09-17 15:39:37 +1200

    add check for file existence

--- genBase64.sh
+++ genBase64.sh
@@ -1,1 +1,8 @@
-echo -n "$(<clientId):$(<clientSecret)" | base64 -w 0 > base64client
+if [ -e clientId && -e clientSecret ]
+then
+	echo -n "$(<clientId):$(<clientSecret)" | base64 -w 0 > base64client
+else
+	echo "make sure you have the files 'clientId' & 'clientSecret' created and populated with your values from the spotify api dashboard. Ensure there is no newline at the end of either of the files."
+fi
+
+
